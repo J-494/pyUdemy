298789c09e46d165cae765daedfa05f67cd563be
Jian494 <68671153+Jian494@users.noreply.github.com>
2021-02-02 22:24:01 +0800

    Update udemyGoogle.py

--- udemyGoogle.py
+++ udemyGoogle.py
@@ -17,10 +17,6 @@ password.send_keys('password')           #password
 driver.find_element_by_name("submit").click()
 
 page2 = 1        
-url = "https://www.tutorialbar.com/page/"+ str(page2)
-page = requests.get(url)    
-soup = BeautifulSoup(page.content, 'html.parser')        
-links = soup.find_all('a')
 countpage = 1
 count = 1
 countcourse = 1
@@ -59,11 +55,42 @@ while True:
                 countfailed += 1
             count += 3            
             countcourse += 1
-            if course == 11:
-                page2 += 1
-                count = 1
-                print("Total Courses Claimed :" + str(countcomplete))
-                print("Total Courses Failed :" + str(countfailed))
+            if course == 11:                
+                count = 0
+                for course2 in range(15):                    
+                    url = "https://www.discudemy.com/all/"+ str(page2)
+                    page = requests.get(url)    
+                    soup = BeautifulSoup(page.content, 'html.parser')        
+                    links = soup.find_all('a',class_="card-header")
+                    x = links[count].get('href')
+                    txt = x.split("/")
+                    try:
+                        print(str(countcourse) + " Trying " + txt[4])
+                    except:
+                        pass       
+                    nurl = "https://www.discudemy.com/go/"+txt[4]
+                    npage = requests.get(nurl)    
+                    nsoup = BeautifulSoup(npage.content, 'html.parser')        
+                    nlinks = nsoup.find('div',class_="ui attached segment").find('a')                    
+                    driver.get(nlinks.get("href"))
+                    time.sleep(5) 
+                    enroll1 = driver.find_elements_by_xpath("//button[@data-purpose='buy-this-course-button']")[0]
+                    enroll1.click()
+                    try:        
+                        time.sleep(5)    
+                        enroll3 = driver.find_element_by_xpath("//*[@class=\"udemy pageloaded\"]/div[1]/div[2]/div/div/div/div[2]/form/div[2]/div/div[4]/button")
+                        enroll3.click()     
+                        countcomplete += 1
+                    except:            
+                        print("It is not free or already be claimed.\n")
+                        countfailed += 1
+                    count += 1
+                    countcourse += 1
+                    if course2 == 14:
+                        page2 += 1
+                        count = 1
+                        print("Total Courses Claimed :" + str(countcomplete))
+                        print("Total Courses Failed :" + str(countfailed))
     except:
         count = 1
         page2 += 1
